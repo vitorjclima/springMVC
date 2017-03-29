857ba3a1458cb1af469d9be6c9c5c2309cf2ab6a
vitorjclima <vitor.lima@unifesp.br>
2017-03-29 16:16:14 -0300

    Insert form in GuestList.html

---
+++ target/classes/import.sql
@@ -0,0 +1,6 @@
+insert into guest (id, name, numberOfGuest)
+values (1, 'Pedro', 2);
+insert into guest (id, name, numberOfGuest)
+values (2, 'Maria', 3);
+insert into guest (id, name, numberOfGuest)
+values (3, 'Ricardo', 1);
