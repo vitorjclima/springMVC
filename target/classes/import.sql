3f82d7f4a4e98290f2997d88b1a31bbc7af782df
vlima <0vitorjclima0@gmail.com>
2017-03-31 15:47:19 -0300

    Fix problem for the attribute date and remove comments from file application.properties

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,3 +1,3 @@
-insert into guest (id, name, number_Of_Guests) values (1, 'Pedro', 2);
-insert into guest (id, name, number_Of_Guests) values (2, 'Maria', 3);
-insert into guest (id, name, number_Of_Guests) values (3, 'Ricardo', 1);
+insert into guest (id, name, number_Of_Guests, date) values (1, 'Pedro', 2, '2015-01-24');
+insert into guest (id, name, number_Of_Guests, date) values (2, 'Maria', 3, '2015-01-24');
+insert into guest (id, name, number_Of_Guests, date) values (3, 'Ricardo', 1, '2015-01-24');
