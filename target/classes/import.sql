0575bbe64f6fa5b6f8712a7df8a13cc24a86eab5
Vitor Lima <vitor.lima@unifesp.br>
2017-03-31 16:24:29 -0300

    Remove folder target

--- target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-insert into guest (id, name, number_Of_Guests, date) values (1, 'Pedro', 2, '2015-01-24');
-insert into guest (id, name, number_Of_Guests, date) values (2, 'Maria', 3, '2015-01-24');
-insert into guest (id, name, number_Of_Guests, date) values (3, 'Ricardo', 1, '2015-01-24');
